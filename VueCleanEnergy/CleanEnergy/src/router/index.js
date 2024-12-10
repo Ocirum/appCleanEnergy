748e621385c6eeb1c293728ce21769567ae8079b
Ocirum <maurlpaz17@gmail.com>
2024-12-09 19:27:20 -0500

    Ultimos arreglos

--- VueCleanEnergy/CleanEnergy/src/router/index.js
+++ VueCleanEnergy/CleanEnergy/src/router/index.js
@@ -4,13 +4,15 @@ import Home from "@/view/Home.vue";
 import Usuario from "@/view/Usuario.vue";
 import TipoEnergia from "@/view/TipoEnergia.vue";
 import ConsumoEnergia from "@/view/ConsumoEnergia.vue";
+import Consultar from "@/view/Consultar.vue";
 
 // Definimos las rutas que aparecen en la barra de navegación
 const routes = [
     { path: '/', name:'home', component: Home },
     { path: '/usuario', name:'usuario', component: Usuario },
     { path: '/tipoEnergia', name:'tipo-energia', component: TipoEnergia },
-    { path: '/consumoEnergia', name:'consumo-energia', component: ConsumoEnergia }, 
+    { path: '/consumoEnergia', name:'consumo-energia', component: ConsumoEnergia },
+    { path: '/consultar', name: 'consultar', component: Consultar }
 ]
 
 // Creamos y exportamos el router para utilizarlo en nuestro main.js
